4f8aefbe362f03b5a2167537073b4d01a1a9e90d
HernandezEduin <eduin.ee08@nycu.edu.tw>
2025-08-25 16:10:17 +0800

    Seeds: Ensuring Transformers Receive Seeds

--- multihopkg/utils/setup.py
+++ multihopkg/utils/setup.py
@@ -5,9 +5,11 @@ def set_seeds(seed):
     import torch
     import numpy as np
     import random
+    import transformers
 
     random.seed(seed)
     np.random.seed(seed)
+    transformers.set_seed(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
